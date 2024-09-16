02c69c7592dc9c75ee2fb6b175700f13ce7e67ca
Eliza Pant <elizapant.github@gmail.com>
2024-09-16 15:59:01 +0545

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello there</h1>
+      <h1 className="text-3xl text-white underline">Hello there</h1>
+      <Button>Click me</Button>
     </div>
   )
 }
